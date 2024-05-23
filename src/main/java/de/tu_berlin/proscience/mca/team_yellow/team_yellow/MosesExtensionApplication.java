52e5498bb14c99e886cba7eb8b54d8b1f6405ca6
gretamagkekse <g.moeller@tu-berlin.de>
2024-05-23 18:59:42 +0200

    initial setup

---
+++ src/main/java/de/tu_berlin/proscience/mca/team_yellow/team_yellow/MosesExtensionApplication.java
@@ -0,0 +1,13 @@
+package de.tu_berlin.proscience.mca.team_yellow.team_yellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosesExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosesExtensionApplication.class, args);
+	}
+
+}
