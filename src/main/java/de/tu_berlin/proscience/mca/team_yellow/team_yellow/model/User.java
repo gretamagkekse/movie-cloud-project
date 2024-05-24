f4f54c5fa713a473fee4a9405906a28c12d306e2
tanembasaraner <tanembasaraner@outlook.com>
2024-05-24 12:46:12 +0200

    create my branch

--- src/main/java/de/tu_berlin/proscience/mca/team_yellow/team_yellow/model/User.java
+++ src/main/java/de/tu_berlin/proscience/mca/team_yellow/team_yellow/model/User.java
@@ -2,4 +2,7 @@ package de.tu_berlin.proscience.mca.team_yellow.team_yellow.model;
 
 public class User {
     //id,display_name, email, degree_id, pw_hash
+
+    //https://github.com/freitagsrunde/modulist/blob/master/db/model-user.go
+    //ID, FirstName,LastName, Mail, MailVerified, PasswordHash, StatusGroup,privileges, Enabled
 }
