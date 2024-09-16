81a82653952b97cdf9be0ac5f0e2be9f5d921f9a
gretamagkekse <g.moeller@tu-berlin.de>
2024-09-16 16:23:00 +0200

    short fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
---bcrypt.online tanem
-INSERT INTO platform_user (user_name, password_hash) VALUES ('Tanem', '$2y$12$b3dRMr9HWefJuYyuUYeSTODO0Exl31lcT8pn6jo5dTMAzP1LeQfkm') ON CONFLICT DO NOTHING;
---bcrypt.online greta
-INSERT INTO platform_user (user_name, password_hash) VALUES ('Greta', '$2y$12$r19OWflLgKo8FgnuW6brHOc9s5VnKYNVFmj2eKtZ.0UFP5j2vhYSC') ON CONFLICT DO NOTHING;
 
 --bcrypt.online tanem
 INSERT INTO platformuser (user_name, passwordhash) VALUES ('Tanem', '$2y$12$b3dRMr9HWefJuYyuUYeSTODO0Exl31lcT8pn6jo5dTMAzP1LeQfkm') ON CONFLICT DO NOTHING;
